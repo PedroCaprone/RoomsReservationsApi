4376c53f780d12ebfbe84020614bb3fc634853b1
PedroCaprone <129163662+PedroCaprone@users.noreply.github.com>
2026-04-16 02:10:35 +0200

    Add POST /api/reservations endpoint

--- RoomsReservationsApi/Controllers/ReservationsController.cs
+++ RoomsReservationsApi/Controllers/ReservationsController.cs
@@ -20,4 +20,13 @@ public class ReservationsController : ControllerBase
     {
         return Ok(_context.Reservations);
     }
+    
+    [HttpPost]
+    public ActionResult<Reservation> AddReservation(Reservation reservation)
+    {
+        reservation.Id = _context.Reservations.Count + 1;
+        _context.Reservations.Add(reservation);
+
+        return CreatedAtAction(nameof(GetReservations), new { id = reservation.Id }, reservation);
+    }
 }
